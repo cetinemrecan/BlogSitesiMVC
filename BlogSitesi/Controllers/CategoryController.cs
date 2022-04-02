d314a0373bed46403cc9ed756b0cc257dcf1f1c7
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-02 03:41:39 +0300

    Tema eklendi

--- BlogSitesi/Controllers/CategoryController.cs
+++ BlogSitesi/Controllers/CategoryController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EF;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogSitesi.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+
+            return View(values);
         }
     }
 }
