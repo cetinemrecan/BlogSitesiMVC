d38dfc19233777b151bbe292269ff06e58214f51
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-06 04:09:03 +0300

    kayıt alanı düzenlemeleri yapıldı

---
+++ BlogSitesi/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EF;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        BloggerManager bm = new BloggerManager(new EfBloggerRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Blogger b)
+        {
+            b.BloggerStatus = true;
+            b.BloggerAbout = "Deneme Test";
+            bm.BloggerAdd(b);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/BloggerManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BloggerManager:IBloggerService
+    {
+        IBloggerDal _bloggerDal;
+
+        public BloggerManager(IBloggerDal bloggerDal)
+        {
+            _bloggerDal = bloggerDal;
+        }
+
+        public void BloggerAdd(Blogger blogger)
+        {
+            _bloggerDal.Insert(blogger);
+        }
+    }
+}
