2392a0cf73eb85a53af4e485e4069f2406678566
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-11 02:34:09 +0300

    contact kısmı eklendi ve güncellendi

--- DataAccessLayer/EF/EfContactRepository.cs
+++ DataAccessLayer/EF/EfContactRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EF
 {
-    internal class EfContactRepository: GenericRepository<Contact>, IContactDal
+    public class EfContactRepository: GenericRepository<Contact>, IContactDal
     {
     }
 }
