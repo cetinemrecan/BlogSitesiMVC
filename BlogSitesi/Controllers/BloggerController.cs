43aba362f2dc055f2c674fd28fc8095f110acfae
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-11 04:43:53 +0300

    Login islemleri alanı duzenlemeleri yapıldı

---
+++ BlogSitesi/Controllers/BloggerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSitesi.Controllers
+{
+   
+
+    public class BloggerController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BloggerProfile()
+        {
+            return View();
+        }
+        
+        public IActionResult BloggerMail()
+        {
+            return View();
+        }
+    }
+}
--- BlogSitesi/Controllers/LoginController.cs
+++ BlogSitesi/Controllers/LoginController.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BlogSitesi.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
-        
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task< IActionResult> Index(Blogger p)
+        {
+          Context c=new Context();
+            var datavalue = c.Bloggers.FirstOrDefault(x => x.BloggerMail == p.BloggerMail && x.BloggerPassword == p.BloggerPassword);
+            if (datavalue != null)
+            {
+                var claims = new List<Claim>() //claim=> talep
+                {
+                    new Claim(ClaimTypes.Name, p.BloggerMail)
+                };
+                var useridentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index", "Blogger");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
     }
 }
+
+//Context c = new Context();
+//var datavalue = c.Bloggers.FirstOrDefault(x => x.BloggerMail == p.BloggerMail && x.BloggerPassword == p.BloggerPassword);
+//if (datavalue != null)
+//{
+//    HttpContext.Session.SetString("username", p.BloggerMail);
+//    return RedirectToAction("Index", "Blogger");
+//}
+//else
+//{
+//    return View();
+//}
