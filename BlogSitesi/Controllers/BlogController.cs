d1665de8f0fc9965b3cf994ff7da5a38806f5188
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-13 03:36:40 +0300

    yazar blog ekleme alanı olusturuldu

--- BlogSitesi/Controllers/BlogController.cs
+++ BlogSitesi/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.Validation;
 using DataAccessLayer.EF;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace BlogSitesi.Controllers
 {
@@ -26,6 +30,34 @@ namespace BlogSitesi.Controllers
           var values= bm.GetBlogListByBlogger(1);
             return View(values);
         }
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator blv = new BlogValidator();
+            ValidationResult result = blv.Validate(p);
+            if (result.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.BloggerID = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByBlogger", "Blog");
 
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+            
+        }
     }
 }
