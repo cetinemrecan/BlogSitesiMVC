29ae60e524559bf7d500728e9be7fe5a484c12d5
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-23 20:05:59 +0300

    sol menu partial view i olusturuldu ve duzenlendi

--- BlogSitesi/Controllers/LoginController.cs
+++ BlogSitesi/Controllers/LoginController.cs
@@ -14,6 +14,7 @@ namespace BlogSitesi.Controllers
     public class LoginController : Controller
     {
         [AllowAnonymous]
+       
         public IActionResult Index()
         {
             return View();
