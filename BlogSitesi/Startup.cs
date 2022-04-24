ff2cc9eb0be8b9a7ece9bc917445a1169f90fd57
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-24 19:44:41 +0300

    areas eklendi

--- BlogSitesi/Startup.cs
+++ BlogSitesi/Startup.cs
@@ -78,9 +78,18 @@ namespace BlogSitesi
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllerRoute(
+               name: "areas",
+               pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+             );
+
+
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
