e6964d4a657df360eb73db740aa82e6e1de2c148
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-04-25 16:49:29 +0300

    kategori ekleme alanı olusturuldu

--- BlogSitesi/Areas/Admin/Controllers/CategoryController.cs
+++ BlogSitesi/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.Validation;
 using DataAccessLayer.EF;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 
@@ -13,6 +16,37 @@ namespace BlogSitesi.Areas.Admin.Controllers
         {
             var values = cm.GetList().ToPagedList(page, 3);
             return View(values);
+        }
+        [HttpGet]
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            
+
+
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(p);
+            if (result.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
