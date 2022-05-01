fdea37a248716d6170aef763dd5fe496c69c3516
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-05-02 01:55:00 +0300

    api eklendi(proje harici denendi)

---
+++ BlogApi/DataAccessLAyer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.DataAccessLAyer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-A0877MT;database=CoreBlogApiDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
