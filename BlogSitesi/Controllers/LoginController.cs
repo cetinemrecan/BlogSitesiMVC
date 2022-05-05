5548b0fb69c8278bf39fd21efad89f369401bc24
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-05-05 19:42:49 +0300

    identity ile login işlemi yapıldı

--- BlogSitesi/Controllers/LoginController.cs
+++ BlogSitesi/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BlogSitesi.Models;
+using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,37 +13,66 @@ using System.Threading.Tasks;
 
 namespace BlogSitesi.Controllers
 {
+    [AllowAnonymous]
+
     public class LoginController : Controller
     {
-        [AllowAnonymous]
-       
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
-        [AllowAnonymous]
-        public async Task< IActionResult> Index(Blogger p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-          Context c=new Context();
-            var datavalue = c.Bloggers.FirstOrDefault(x => x.BloggerMail == p.BloggerMail && x.BloggerPassword == p.BloggerPassword);
-            if (datavalue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>() //claim=> talep
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name, p.BloggerMail)
-                };
-                var useridentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
             }
-            else
-            {
-                return View();
-            }
-            
+            return View();
         }
+
+
+
+
+
+        //[HttpPost]
+        //public async Task< IActionResult> Index(Blogger p)
+        //{
+        //  Context c=new Context();
+        //    var datavalue = c.Bloggers.FirstOrDefault(x => x.BloggerMail == p.BloggerMail && x.BloggerPassword == p.BloggerPassword);
+        //    if (datavalue != null)
+        //    {
+        //        var claims = new List<Claim>() //claim=> talep
+        //        {
+        //            new Claim(ClaimTypes.Name, p.BloggerMail)
+        //        };
+        //        var useridentity = new ClaimsIdentity(claims, "a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+            
+        //}
     }
 }
 
---
+++ BlogSitesi/Models/UserSignInViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogSitesi.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını girin")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+
+        public string password { get; set; }
+    }
+}
