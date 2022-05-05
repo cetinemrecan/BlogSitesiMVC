939f0ee48e09b50b0b7c4e7c85afe795e9d2ad5c
cetinemrecann98@outlook.com <cetinemrecann98@outlook.com>
2022-05-05 18:38:23 +0300

    identity ile register islemi yapıldı

---
+++ BlogSitesi/Models/UserSignUpViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogSitesi.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Lütfen Ad Soyad Giriniz")]
+        public string NameSurname { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Lütfen Şifrenizi Giriniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Compare("Password",ErrorMessage = "Şifreler Uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Mail Adresi")]
+        [Required(ErrorMessage = "Lütfen Mail Giriniz")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+
+        public string UserName { get; set; }
+
+
+    }
+}
